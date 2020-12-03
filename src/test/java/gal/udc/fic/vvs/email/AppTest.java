b1e16ad200418f6c50c6b40c472fd8f1c9458909
Natalia <n.chacon@udc.es>
2020-12-03 15:43:15 +0100

    todas las pruebas unitarias de archivador + No puedo eliminar target

--- src/test/java/gal/udc/fic/vvs/email/AppTest.java
+++ src/test/java/gal/udc/fic/vvs/email/AppTest.java
@@ -5,16 +5,17 @@ import static org.junit.Assert.assertTrue;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Vector;
 
 import org.junit.Test;
 
 import gal.udc.fic.vvs.email.archivador.ArchivadorSimple;
+import gal.udc.fic.vvs.email.archivador.DecoradorArchivador;
+import gal.udc.fic.vvs.email.archivador.Delegado;
+import gal.udc.fic.vvs.email.archivador.Log;
 import gal.udc.fic.vvs.email.archivo.Texto;
 import gal.udc.fic.vvs.email.correo.Carpeta;
 import gal.udc.fic.vvs.email.correo.CarpetaLimitada;
 import gal.udc.fic.vvs.email.correo.Correo;
-import gal.udc.fic.vvs.email.correo.CorreoAbstracto;
 import gal.udc.fic.vvs.email.correo.Mensaje;
 import gal.udc.fic.vvs.email.correo.OperacionInvalida;
 
@@ -41,13 +42,50 @@ public class AppTest {
         
         try {
             correoPadre.añadir(correoHijo);
-            System.out.println("Tam correo: " + correoPadre.obtenerTamaño());
         } catch (OperacionInvalida e) {
             e.printStackTrace();
         }
         ArchivadorSimple archi = new ArchivadorSimple("", 5);
         archi.almacenarCorreo(correoPadre);
         assertEquals(4, archi.obtenerEspacioDisponible());
+    }
+
+    @Test
+    public void testDelegadoAlmacenarCorreo() {
+        Texto contenido = new Texto("nombre","c");
+        Carpeta carpeta = new Carpeta("nombre");
+
+        Correo correoHijo = new Mensaje(contenido);
+        Correo correoPadre = new CarpetaLimitada(carpeta, 1);
+        
+        try {
+            correoPadre.añadir(correoHijo);
+        } catch (OperacionInvalida e) {
+            e.printStackTrace();
+        }
+        ArchivadorSimple archi = new ArchivadorSimple("", 5);
+        Delegado delegado = new Delegado(archi);
+        delegado.almacenarCorreo(correoPadre);
+        assertEquals(4, delegado.obtenerEspacioDisponible());
+    }
+
+    @Test
+    public void testLogAlmacenarCorreo() {
+        Texto contenido = new Texto("nombre","c");
+        Carpeta carpeta = new Carpeta("nombre");
+
+        Correo correoHijo = new Mensaje(contenido);
+        Correo correoPadre = new CarpetaLimitada(carpeta, 1);
+        
+        try {
+            correoPadre.añadir(correoHijo);
+        } catch (OperacionInvalida e) {
+            e.printStackTrace();
+        }
+        ArchivadorSimple archi = new ArchivadorSimple("", 5);
+        Log log = new Log(archi);
+        boolean si = log.almacenarCorreo(correoPadre);
+        assertEquals(true, si);
     }
 
     //Archivo
